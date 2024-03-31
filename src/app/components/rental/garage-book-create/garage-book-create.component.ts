b85bb5e52488debe88b5aae14ef9146a2bbdc06b
3ralon <albperleo@alum.us.es>
2024-03-31 22:42:07 +0200

    fix: create book user id and enum tags #176

--- src/app/components/rental/garage-book-create/garage-book-create.component.ts
+++ src/app/components/rental/garage-book-create/garage-book-create.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ModalController, NavParams, ToastController } from '@ionic/angular';
 
@@ -14,7 +14,7 @@ import { RestService } from 'src/app/service/rest.service';
   selector: 'app-garage-book-create',
   templateUrl: './garage-book-create.component.html',
 })
-export class GarageBookCreateComponent {
+export class GarageBookCreateComponent implements OnInit {
   garageId!: string;
   currentGarage!: Garage;
   baseUrl = '/G11/aparKing/garages';
@@ -35,15 +35,25 @@ export class GarageBookCreateComponent {
     this.bookForm = new FormGroup({
       payment_method: new FormControl(PaymentMethod.CARD, Validators.required),
       status: new FormControl(BookingStatus.CONFIRMED, Validators.required),
-      user: new FormControl(this.dataManagement.userId, Validators.required),
+      user: new FormControl(
+        this.dataManagement.userId.getValue(),
+        Validators.required
+      ),
       availability: new FormControl(1, Validators.required),
     });
     this.payment_methods = Object.values(PaymentMethod);
+  }
 
+  ngOnInit(): void {
     this.restService
       .getAvailabilitiesByGarageId(this.garageId)
       .then((availabilities) => {
-        this.availabilities = availabilities;
+        this.availabilities = availabilities.filter((availability: any) => {
+          return availability.status === 'Disponible';
+        });
+        if (this.availabilities.length === 0) {
+          this.cancel();
+        }
       })
       .catch((_) => {
         this.cancel();
@@ -51,6 +61,21 @@ export class GarageBookCreateComponent {
   }
 
   async cancel() {
+    const toast = await this.toastController.create({
+      duration: 2000, // Duración del toast en milisegundos
+      position: 'bottom', // Posición del toast (top, middle, bottom)
+      color: 'dark', // Color del toast
+      buttons: [
+        {
+          text: 'Cerrar',
+          role: 'cancel',
+        },
+      ],
+    });
+
+    toast.message = 'No hay disponibilidad en este momento';
+    toast.present();
+
     return this.modalCtrl.dismiss(null, 'cancel');
   }
 
@@ -67,8 +92,23 @@ export class GarageBookCreateComponent {
       ],
     });
 
+    const bookingFormValue = this.bookForm.value;
+
+    const translatedPaymentMethod = this.translatePaymentMethod(
+      bookingFormValue.paymentMethod
+    );
+    const translatedStatus = this.translateBookingStatus(
+      bookingFormValue.status
+    );
+
+    const bookingData = {
+      ...bookingFormValue,
+      payment_method: translatedPaymentMethod,
+      status: translatedStatus,
+    };
+
     this.restService
-      .createBooking(this.bookForm.value)
+      .createBooking(bookingData)
       .then((_) => {
         toast.message = 'Reserva creada correctamente';
         toast.present();
@@ -77,6 +117,31 @@ export class GarageBookCreateComponent {
         toast.message = 'Error al crear la reserva';
         toast.present();
       });
+
     return this.modalCtrl.dismiss(null, 'confirm');
+  }
+
+  translatePaymentMethod(paymentMethod: string): string {
+    switch (paymentMethod) {
+      case 'Efectivo':
+        return 'CASH';
+      case 'Tarjeta':
+        return 'CARD';
+      default:
+        return 'CASH';
+    }
+  }
+
+  translateBookingStatus(status: string): string {
+    switch (status) {
+      case 'Pendiente':
+        return 'PENDING';
+      case 'Confirmada':
+        return 'CONFIRMED';
+      case 'Cancelada':
+        return 'CANCELLED';
+      default:
+        return 'CONFIRMED';
+    }
   }
 }
