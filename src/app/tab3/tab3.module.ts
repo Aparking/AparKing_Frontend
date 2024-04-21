1584fe196a6a8585df38a9607e29d824379785cd
3ralon <albperleo@alum.us.es>
2024-04-21 18:15:15 +0200

    feat: init profile component #5
    Co-authored-by: VIRGILIO OLIVA ALONSO <virolialo@alum.us.es>

--- src/app/tab3/tab3.module.ts
+++ src/app/tab3/tab3.module.ts
@@ -1,13 +1,16 @@
 import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { Tab3Page } from './tab3.page';
 
+import { ProfileComponent } from '../components/profile/profile.component';
 import { Tab3PageRoutingModule } from './tab3-routing.module';
 
 @NgModule({
   imports: [IonicModule, CommonModule, FormsModule, Tab3PageRoutingModule],
-  declarations: [Tab3Page],
+  declarations: [Tab3Page, ProfileComponent],
+  exports: [ProfileComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class Tab3PageModule {}
