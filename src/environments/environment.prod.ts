769eb36f77de351be29c92275822452f9daec355
Juan Carlos Ramírez López <juancarlosralop@gmail.com>
2024-04-21 14:46:10 +0200

    deploy: ip

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  wsUrl: 'ws://34.175.16.117:3000/ws/parking/',
-  restUrl: 'http://34.175.16.117:3000',
+  wsUrl: 'ws://34.175.243.212:3000/ws/parking/',
+  restUrl: 'http://34.175.243.212:3000',
   APP_NAME: 'AparKing',
 };
