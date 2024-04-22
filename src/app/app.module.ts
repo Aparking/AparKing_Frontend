df3e4365abc061b0768329932526290240d2af71
AparKingG11 <72879200+CarmenRuizPorcel@users.noreply.github.com>
2024-04-22 19:49:29 +0200

    Fix: Deploy vercel, double HttpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import {
