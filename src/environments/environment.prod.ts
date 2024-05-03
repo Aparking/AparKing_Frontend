42fc233ad1386a8c86967470bb26ac8ec7c06554
sergiosant02 <sergiosantiago0403@gmail.com>
2024-05-03 17:08:57 +0200

    fix: change ip for deploy #313

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  wsUrl: 'ws://34.175.255.82:3000/ws/parking/',
-  restUrl: 'http://34.175.255.82:3000',
+  wsUrl: 'ws://aparking-g11-s3.oa.r.appspot.com/ws/parking/',
+  restUrl: 'https://aparking-g11-s3.oa.r.appspot.com',
   APP_NAME: 'AparKing',
 };
