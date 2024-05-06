3e9a08850d929dfc2a3481911ac3de80a1971a43
alepervaz <alepervaz@us.es>
2024-05-06 20:25:12 +0200

    fix: edicion de perfil de usuario

--- src/app/service/rest.service.ts
+++ src/app/service/rest.service.ts
@@ -67,7 +67,7 @@ export class RestService extends WsAbstractService {
   }
 
   async updateUser(data: User): Promise<any> {
-    return await this.makePostRequest(`${this.path}/user/profile`, data);
+    return await this.makePutRequest(`${this.path}/user/profile/`, data);
   }
   async getAllGarages(): Promise<Garage[]> {
     return await this.makeGetRequest(`${this.path}/garages/`);
