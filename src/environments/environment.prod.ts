b5d7e24e31fdcb8b49f66d9a7a43ecbd9d3ab171
sergiosant02 <sergiosantiago0403@gmail.com>
2024-05-20 20:30:14 +0200

    config deploy url for wpl #370

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  wsUrl: 'wss://aparking-g11-s4.oa.r.appspot.com/ws/parking/',
-  restUrl: 'https://aparking-g11-s4.oa.r.appspot.com',
+  wsUrl: 'wss://aparking-g11-wpl.oa.r.appspot.com/ws/parking/',
+  restUrl: 'https://aparking-g11-wpl.oa.r.appspot.com',
   APP_NAME: 'AparKing',
 };
